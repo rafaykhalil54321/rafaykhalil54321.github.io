81551c32d65e0cd5c332853c013bd46195eb40d0
rafaykhalil54321 <ka097799@gmail.com>
2025-03-16 02:24:46 +0500

    Delete script.js
    Vbtbt

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Show Google Form when button is clicked
-    document.getElementById("registerButton").addEventListener("click", function () {
-        document.getElementById("registrationForm").classList.remove("hidden");
-    });
-});
