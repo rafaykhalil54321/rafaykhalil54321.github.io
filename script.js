a269b21ecd162e532a990aa31fed2ac3b14e51ea
rafaykhalil54321 <ka097799@gmail.com>
2025-03-15 23:05:59 +0500

    Create script.js
    Rafay

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Show Google Form when button is clicked
+    document.getElementById("registerButton").addEventListener("click", function () {
+        document.getElementById("registrationForm").classList.remove("hidden");
+    });
+});
